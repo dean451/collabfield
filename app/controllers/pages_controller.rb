19567f302acb797e286b7d67badb8344291b51c5
Dean Palm <dean@sxsw.com>
2018-08-18 18:17:48 -0500

    mirroring the airtable sheet

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
 
-  def worksheep
+  def app
     {
       "Rails": "appOxj990ARAtO1pS",
       "Other Worksheet": "other key here"
@@ -9,7 +9,7 @@ class PagesController < ApplicationController
 
   def index
     # @client = Airtable::Client.new(ENV['AIRTABLE_API_KEY'])
-    @table = HTTParty.get("https://api.airtable.com/v0/#{app['Rails']}/Table%201?api_key=#{ENV['AIRTABLE_API_KEY']}")
+    @table = HTTParty.get("https://api.airtable.com/v0/#{app[:Rails]}/Table%201?api_key=#{ENV['AIRTABLE_API_KEY']}")
     @records= @table['records']
     # raise 'hi'
     # @records.offset
