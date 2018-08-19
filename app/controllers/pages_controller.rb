d7ae6cf3dda02aa8e2c0666bb1d2c08dc5021316
Dean Palm <dean@sxsw.com>
2018-08-18 21:45:31 -0500

    cache it

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
 
+  def get()
+    Rails.cache.fetch(:expires => 1.hour) do
+      HTTParty.get("https://api.airtable.com/v0/#{app[:Rails]}/Table%201?api_key=#{ENV['AIRTABLE_API_KEY']}")
+    end
+  end
+
   def app
     {
       "Rails": "appOxj990ARAtO1pS",
@@ -7,9 +13,11 @@ class PagesController < ApplicationController
     }
   end
 
+
+
   def index
     # @client = Airtable::Client.new(ENV['AIRTABLE_API_KEY'])
-    @table = HTTParty.get("https://api.airtable.com/v0/#{app[:Rails]}/Table%201?api_key=#{ENV['AIRTABLE_API_KEY']}")
+    @table = get
     @records= @table['records']
 
     @bill = @records.first
