bbbbd1ec7e68ed7c9135a569aa11fbaa754b0677
Dean Palm <dean@sxsw.com>
2018-09-17 22:08:41 -0500

    pulled the airtable stuff into my airfable repo, look there

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,26 +1,4 @@
 class PagesController < ApplicationController
 
-  def get()
-    Rails.cache.fetch(:expires => 1.hour) do
-      HTTParty.get("https://api.airtable.com/v0/#{app[:Rails]}/Table%201?api_key=#{ENV['AIRTABLE_API_KEY']}")
-    end
-  end
-
-  def app
-    {
-      "Rails": "appOxj990ARAtO1pS",
-      "Other Worksheet": "other key here"
-    }
-  end
-
-
-
-  def index
-    # @client = Airtable::Client.new(ENV['AIRTABLE_API_KEY'])
-    @table = get
-    @records= @table['records'].sort_by { |hsh| (hsh["fields"]["Name"]).length }
-    @bill = @records.first
-
-  end
-
+  
 end
