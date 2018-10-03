a8b636fbb602369c084bef655bfd0b4f3b32fe9f
Dean Palm <dean451@gmail.com>
2018-10-03 17:30:55 -0500

    not getting bogged down with tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,8 +11,6 @@ require 'capybara/poltergeist'
 require 'factory_girl_rails'
 require 'capybara/rspec'
 
-Capybara.javascript_driver = :poltergeist
-Capybara.server = :puma
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -42,6 +40,9 @@ RSpec.configure do |config|
 
   config.include Devise::Test::IntegrationHelpers, type: :feature
   config.include FactoryGirl::Syntax::Methods
+
+  Capybara.javascript_driver = :poltergeist
+  Capybara.server = :puma
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
