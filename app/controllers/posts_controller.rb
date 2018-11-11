4390e0e5aa942495c41385f066d0297a47d8ca1f
Dean Palm <dean451@gmail.com>
2018-11-11 01:20:06 -0600

    action cable messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,9 @@ before_action :redirect_if_not_signed_in, only: [:new]
 
   def show
     @post = Post.find(params[:id])
+    if user_signed_in?
+      @message_has_been_sent = conversation_exist?
+    end
   end
 
   def hobby
@@ -58,6 +61,10 @@ before_action :redirect_if_not_signed_in, only: [:new]
   def post_params
     params.require(:post).permit(:content, :title, :category_id)
                          .merge(user_id: current_user.id)
+  end
+
+  def conversation_exist?
+    Private::Conversation.between_users(current_user.id, @post.user.id).present?
   end
 
 
