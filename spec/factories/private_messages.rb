6bd8dd247a706d3820317b9dbcc9ea160455c29d
Dean Palm <dean451@gmail.com>
2018-12-10 11:35:37 -0600

    Revert "sending messages"
    This reverts commit 3e1756ff76105f5c55cc41ef7f70cbbe571fb344.

--- spec/factories/private_messages.rb
+++ spec/factories/private_messages.rb
@@ -1,7 +1,5 @@
 FactoryGirl.define do
   factory :private_message, class: 'Private::Message' do
-    body 'a' * 20
-    association :conversation, factory: :private_conversation
-    user
+    
   end
 end
